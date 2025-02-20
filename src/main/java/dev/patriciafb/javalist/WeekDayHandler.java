9a8f2840f90110041a105d70b0db00cbf125edda
PFBregon <bregon@gmail.com>
2025-02-20 22:55:06 +0100

    getDaysTest run ok

--- src/main/java/dev/patriciafb/javalist/WeekDayHandler.java
+++ src/main/java/dev/patriciafb/javalist/WeekDayHandler.java
@@ -4,8 +4,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class WeekDayHandler {
+    private List<String> weekDays;
+
     public List<String> createList() {
         return Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
     }
+
+
+public List<String> getDays() {
+      return weekDays();
+          }
+
+
+      private List<String> weekDays() {
+        this.weekDays = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+        return weekDays;
+      }
 
 }
